247b95cd364b323e67e6e973a342df3bb50114bf
Weslley Barros <weslley17w@gmail.com>
2022-09-02 14:46:06 -0300

    add method Cadastrar()

--- src/controller/Jogador.java
+++ src/controller/Jogador.java
@@ -5,7 +5,10 @@ import javax.swing.table.DefaultTableModel;
 public class Jogador {
 
 
-
+	private boolean passaporteEuropeu;
+	private float   salario;
+	private float   multa;
+	private int     qntGols;
 	private model.Database db = model.Database.getInstance();
 
 	public DefaultTableModel gerarLista() {
@@ -21,6 +24,45 @@ public class Jogador {
 		});
 
 		return modeloTabelaJogador;
+	}
+	
+	public int Cadastrar(String nome, String nacionalidade,String dtNascimento, String salario, String multa, String qntGols, String passaporteEuropeu) {
+		int saida = 0;
+
+
+		if (passaporteEuropeu == "Sim") {
+			this.passaporteEuropeu = true;
+		} else {
+			this.passaporteEuropeu = false;
+		}
+
+
+		try {
+			this.salario = Float.parseFloat(salario);
+			this.multa = Float.parseFloat(multa);
+		} catch (NumberFormatException ex) {
+			saida = 3;
+		}
+
+		try {
+			this.qntGols = Integer.parseInt(qntGols);
+		} catch (final NumberFormatException e) {
+			saida = 4;
+		}
+		
+		if (nome.isEmpty() || dtNascimento.isEmpty() || nacionalidade.isEmpty() || salario.isEmpty() || multa.isEmpty()
+				|| multa.isEmpty() || qntGols.isEmpty() || passaporteEuropeu.isEmpty()) {
+			saida = 2;
+		}
+
+		if (saida == 0) {
+			model.Jogador jogador = new model.Jogador(nome,nacionalidade,dtNascimento, this.salario, this.multa, this.qntGols, this.passaporteEuropeu);
+			this.db.adicionarJogador(jogador);
+		}
+		
+		
+
+		return saida;
 	}
 
 	
